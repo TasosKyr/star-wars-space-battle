8b3b860ae40c9ddd482b615656fbb98fbf5bebf0
Tasos Kyr <tasoskyr1@gmail.com>
2019-04-01 17:28:51 +0200

    Sith character creation and move

--- battle.js
+++ battle.js
@@ -3,10 +3,12 @@ let backImage;
 let enemiesImages = [];
 let enemiesArray = [];
 let bulletsArray = [];
+let velocitySith;
 class Battle {
     constructor() {
         this.spaceship = new Spaceship();
         this.bullet = new Bullet();
+        this.sith = new Sith();
         //this.enemy = new Enemy();
     }
     setup() {
@@ -19,16 +21,17 @@ class Battle {
         this.bullet.setup();
         for (let i = 0; i < 8; i++) {
             for (let y = 0; y < 4; y++) {
-                enemiesArray[i] = new Enemy(40 + 110 * i, 90);
+                enemiesArray.push(new Enemy(40 + 110 * i, 80 + 80 * y));
             }
         }
         enemiesArray.forEach(enemy => enemy.setup());
+        this.sith.setup();
     }
 
     draw() {
         this.spaceship.draw();
         this.bullet.draw();
         enemiesArray.forEach(enemy => enemy.draw());
-        //this.enemy.draw();
+        this.sith.draw();
     }
 }
--- enemies.js
+++ enemies.js
@@ -4,11 +4,27 @@ class Enemy {
         this.y = y;
     }
     setup() {
-        let randomIm = Math.floor(Math.random() * 8);
+        let randomIm = Math.floor(Math.random() * 6);
         this.img = loadImage('./images/enemy' + randomIm + '.png');
     }
 
     draw() {
         image(this.img, this.x, this.y, this.img.width / 1.5, this.img.height / 1.5);
+    }
+}
+
+class Sith {
+    constructor() {}
+    setup() {
+        this.img = loadImage('./images/sith2.png');
+        this.x = BATTLE_WIDTH / 2;
+        this.y = 5;
+    }
+    draw() {
+        image(this.img, this.x, this.y, this.img.width / 1.5, this.img.height / 1.5);
+        this.x = this.x + 2;
+        if (this.x > BATTLE_WIDTH - 20) {
+            this.x = -10;
+        }
     }
 }
